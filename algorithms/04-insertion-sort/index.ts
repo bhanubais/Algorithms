0470c073515cd348e3b5ba7f2df3bd7c9a00190e
bhanubais <bhanubais@gmail.com>
2026-05-13 17:17:13 +0530

    algorithm: Insertion sort implemented

--- algorithms/04-insertion-sort/index.ts
+++ algorithms/04-insertion-sort/index.ts
@@ -1,3 +1,21 @@
 export default function insertion_sort(arr: number[]): void {
 	// TODO: Implement the insertion sort algorithm here (sort in-place).
+	const n = arr.length;
+	let key: number;
+
+	// iterate i -> [1 to n)
+	for (let i = 1; i < n; i++) {
+		key = arr[i];
+
+		// iterate j until correct position of the key
+		let j = i - 1;
+		while (0 <= j && key < arr[j]) {
+			// shift element to right-side
+			arr[j + 1] = arr[j];
+			j--;
+		}
+
+		// place key element to it's correct position
+		arr[j + 1] = key;
+	}
 }
--- algorithms/04-insertion-sort/main.py
+++ algorithms/04-insertion-sort/main.py
@@ -1,3 +1,16 @@
 def insertion_sort(arr: list[int]) -> None:
-    # TODO: Implement the insertion sort algorithm here (sort in-place).
-    pass
+    # Implement the insertion sort algorithm here (sort in-place).
+    n = len(arr)
+
+    # iterate i -> [1 to n)
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+
+        # iterate j until correct position of the key
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        # place key at it's correct position
+        arr[j + 1] = key
