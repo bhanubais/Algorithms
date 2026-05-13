94437a4d0fd4a6c5e4197fa52f103b411d0095e0
bhanubais <bhanubais@gmail.com>
2026-05-13 16:17:51 +0530

    algorithm: bubble sort implemented

--- algorithms/03-bubble-sort/index.ts
+++ algorithms/03-bubble-sort/index.ts
@@ -1,4 +1,21 @@
 export function bubble_sort(arr: number[]): void {
 	// TODO: Implement the O(n^2) bubble sort algorithm.
 	// Sort the array in-place. Do not return a new array.
+	const n = arr.length;
+	let swapped = false;
+
+	// iterate i -> [n-1 to 0]
+	for (let i = n - 1; i >= 0; i--) {
+		swapped = false;
+		// iterate j -> [0 to i)
+		for (let j = 0; j < i; j++) {
+			// swap if require
+			if (arr[j + 1] < arr[j]) {
+				[arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
+				swapped = true;
+			}
+		}
+		// already sorted
+		if (!swapped) break;
+	}
 }
--- algorithms/03-bubble-sort/main.py
+++ algorithms/03-bubble-sort/main.py
@@ -1,5 +1,26 @@
 def bubble_sort(arr: list[int]) -> None:
-    # TODO: Implement the O(n^2) bubble sort algorithm.
+    # Implement the O(n^2) bubble sort algorithm.
     # Sort the array in-place. Do not return a new array.
+    n = len(arr)
+    swapped: bool
 
-    pass
+    # iterate i -> [n-1 to 0]
+    for i in range(n - 1, -1, -1):
+        swapped = False
+        # iterate j -> [0 to i)
+        for j in range(i):
+            # print(arr[j], arr[j + 1], " -> ", arr)
+            # swap if require
+            if arr[j + 1] < arr[j]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+
+        # already sorted
+        if not swapped:
+            break
+
+
+# Test
+arr = [4, 2, 7, 4, 1, 9, 3]
+bubble_sort(arr)
+print(arr)
